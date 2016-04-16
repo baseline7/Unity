e9bc02e5720b7605690b37f75a383eb02b1525b7
Caleb <umbrellyfish@gmail.com>
2016-04-15 21:55:42 -0700

    Added basic game files

---
+++ Marble Bleyadst Ultra/Assets/FollowBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Ball;
+public class FollowBall : MonoBehaviour {
+
+    public GameObject player;
+    public float damping;
+    public Ball ball;
+
+	// Use this for initialization
+	void Start () {
+	
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 target = player.transform.position - transform.position;
+        Quaternion lookAt = Quaternion.LookRotation(target);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookAt, damping);
+
+        if (ball.dead == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x - 3, player.transform.position.y + 2, player.transform.position.z), damping / 4);
+        }
+	}
+}
